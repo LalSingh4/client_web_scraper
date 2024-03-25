a44df482016797730f4b143951708ef10fa32a25
Lal Singh <114930901+LalSingh4@users.noreply.github.com>
2024-03-25 18:11:09 +0530

    Update web_service.py

--- web_service.py
+++ web_service.py
@@ -1,17 +1,21 @@
 from scrape import ScrapeSite
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 app = FastAPI()
+class URLInput(BaseModel):
+    url:str
 
 @app.get("/get_movie")
-async def get_movie(url):
+async def get_movie(url_input: URLInput):  #url_unput is instance of URLInput
     #Create instance of ScrapeSite class
-    scrape = ScrapeSite()
+    scrape = ScrapeSite(url_input.url)
     #Call the scrape site
-    movie_list = scrape.scrape_site(url)
+    movie_list = scrape.scrape_site()
     return {"movies_title":movie_list}
 
 
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run(app, host="127.0.0.1", port=8000)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
